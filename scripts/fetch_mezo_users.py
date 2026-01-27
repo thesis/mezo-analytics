a6eb8485978ac77919002597d39194773dd76a80
LJ <lauren.jackson@thesis.co>
2026-01-27 17:25:34 -0500

    Enhance user data processing in fetch_mezo_users.py: added timedelta for date handling, improved function descriptions, and introduced Galxe export functionality. Updated main function to streamline data fetching and processing.

--- scripts/fetch_mezo_users.py
+++ scripts/fetch_mezo_users.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 import os
 
 from dotenv import load_dotenv
@@ -11,8 +11,8 @@ from mezo.visual_utils import ProgressIndicators, with_progress
 # HELPER FUNCTIONS
 # ==================================================
 
-@with_progress("Cleaning user table")
-def clean_users(df, last_active: None):
+@with_progress("Cleaning raw user data")
+def clean_users(df, last_active=None):
     df = df.sort_values(by='updated_at', ascending=False)
     df = df.loc[df['auth_user_id'].notna()].reset_index()
 
@@ -24,38 +24,50 @@ def clean_users(df, last_active: None):
     
     return df
 
-@with_progress("Fetching table from Supabase")
+@with_progress("Fetching user data from accounts table in Supabase")
 def fetch_users(supabase):
     users = supabase.fetch_table_data('accounts')
 
     return users
 
-def fetch_btc_users(users):
-    btc_users = users[users['address'].str.startswith('b', na=False)].reset_index()
-    return btc_users
-
-@with_progress("Saving to csv")
+@with_progress("Saving user data to csv")
 def save_to_csv(df, name):
     os.makedirs('./outputs', exist_ok=True)
-    output_path = f'./outputs/{name}_{date.today()}.csv'
-    df.to_csv(output_path)
-    print(f"Saved CSV to {output_path}")
+    
+    yesterday = date.today() - timedelta(days=1)
+    previous_day_path = f'./outputs/{name}_{yesterday}.csv'
 
-@with_progress("Uploading mezo_users_stg to BigQuery")
+    if os.path.exists(previous_day_path):
+        os.remove(previous_day_path)
+        print(f"Deleted previous day's CSV: {previous_day_path}")
+    
+    output_path = f'./outputs/{name}_{date.today()}.csv'
+    
+    df.to_csv(output_path)
+
+@with_progress("Uploading data to BigQuery")
 def upload_to_bigquery(df, dataset, table, identifier, bq):
     if df is not None and len(df) > 0:
         bq.update_table(df, dataset, table, identifier)
 
+@with_progress("Creating Galxe export")
+def create_galxe_export(df, name):
+    df = df[["address"]].reset_index(drop=True)
+    save_to_csv(df, name)
+
+def get_btc_users(users):
+    btc_users = users[users["address"].str.startswith("b", na=False)].reset_index()
+
+    return btc_users
+
 def print_summary(users, start):
     total_users = users['address'].count()
-    # total_users_with_auth_ids = int_users['address'].count()
-    # all_time_users_with_auth_ids = raw_users['address'].count()
     
     ProgressIndicators.print_summary_box(
         "📊 USERS SUMMARY",
         {
             "Starting date": start,
-            "Total users": total_users,
+            "Total users": f"{total_users:,}"
         }
     )
 
@@ -63,26 +75,32 @@ def print_summary(users, start):
 # RUN MAIN FUNCTION
 # ==================================================
 
-def main():
+def main(skip_bigquery=False, test_mode=False):
     ProgressIndicators.print_header("📌 GET MEZO USER DATA")    
+
+    # set up env and clients
     load_dotenv(dotenv_path='../.env', override=True)
     supabase = SupabaseClient(url='SUPABASE_URL_PROD', key='SUPABASE_KEY_PROD')    
     bq = BigQueryClient(key='GOOGLE_CLOUD_KEY', project_id='mezo-portal-data')
 
+    # set start date for filtering users
     start = '2025-05-28' # mainnet launch
 
-    users = fetch_users(supabase)
-    users_stg = clean_users(users, start)
+    # fetch raw data from supabase `accounts` table
+    users_raw = fetch_users(supabase)
+    create_galxe_export(users_raw, "users_raw")
+
+    # clean the raw users data and create staging tables    
+    users_stg = clean_users(users_raw, start)
     save_to_csv(users_stg, 'users')
 
-    btc_users = fetch_btc_users(users)
-    save_to_csv(btc_users, 'btc_users')
-
+    # bigquery data uploads
+    upload_to_bigquery(users_raw, 'supabase', 'raw_mezo_users', 'auth_user_id', bq)
     upload_to_bigquery(users_stg, 'staging', 'mezo_users_stg', 'auth_user_id', bq)
 
-    print_summary(users, start)
+    print_summary(users_raw, start)
+    
     ProgressIndicators.print_header("🚀 PROCESSING COMPLETED SUCCESSFULLY 🚀")
-
 
 if __name__ == "__main__":
     main()
